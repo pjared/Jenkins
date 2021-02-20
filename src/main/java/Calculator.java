d149a2ae7a84ca714c31efbcdee56db33644690a
pjared <pjared870@gmail.com>
2021-02-20 14:49:13 -0700

    Finished unique string

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -70,8 +71,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        UUID uuid = UUID.randomUUID();
+        return (n + uuid.toString());
     }
 
 
