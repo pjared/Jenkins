58093fffa7ed73e4f64fcef37ae0c7ca31036b90
pjared <pjared870@gmail.com>
2021-02-27 21:36:38 -0700

    pls help me

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
-        if (args.length < 0) {
+        if (args.length < 1) {
             return;
         }
         String funct = args[0];
